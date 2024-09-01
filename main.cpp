49ceb95a73d82a747dc0e8a80bedc35a60b8b563
安波 <anbo@anbodeMacBook-Pro.local>
2024-09-01 22:18:34 +0800

    2024/9/1

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream> // 引入 输入/输出流库
+using namespace std; // 引入命名空间
+#define day 8
+int main(){
+    char a = 'n';
+    cout<<a<<endl; // 输出
+    cout  <<endl<< "这是一个测试程序"<<endl<< "这是一个天数"<<day<<endl<< "Press Enter to continue..."; // 输出
+    cin.get(); // 输出
+    return 0;
+}
