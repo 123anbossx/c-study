c60e5acdc1d50bfcbbc84121f8fd885bb6bbbb28
安波 <anbo@anbodeMacBook-Pro.local>
2024-10-04 10:23:37 +0800

    释放堆中的内存

--- 堆.cpp
+++ 堆.cpp
@@ -5,8 +5,19 @@ int * func(){
     int *p = new int(10);
     return p;
 };
+void creaeArr(){
+   int *arr = new int[10]
+   for(int i=0;i<10;i++){
+      arr[i] = i+100
+   }
+   delete[] arr // 释放一个人数组 需要加一个中括号
+}
 int main(){
     int *a = func();
+    cout << *a << endl;
+    cout << *a << endl;
+    cout << *a << endl;
+    delete a; // 用delete 释放堆中的内存
     cout << *a << endl;
     return 0;
 }
