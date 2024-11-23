c22e47cbf4ec0665c2ffb6b06fc13efe3de3a377
anbo <anbo@runzeyuan.com>
2024-11-23 21:59:34 +0800

    提交

---
+++ 通讯录管理系统/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/boss.cpp.o"
+  "CMakeFiles/main.dir/boss.cpp.o.d"
+  "CMakeFiles/main.dir/employee.cpp.o"
+  "CMakeFiles/main.dir/employee.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "CMakeFiles/main.dir/manager.cpp.o"
+  "CMakeFiles/main.dir/manager.cpp.o.d"
+  "CMakeFiles/main.dir/workerManager.cpp.o"
+  "CMakeFiles/main.dir/workerManager.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
