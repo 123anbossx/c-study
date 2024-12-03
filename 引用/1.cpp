0dec1c152858bca59f47bbb8a66b8c0bbad1b0c6
anbo <anbo@runzeyuan.com>
2024-12-03 20:59:10 +0800

    模板

--- 引用/1.cpp
+++ 引用/1.cpp
@@ -8,14 +8,12 @@ void yinyong(int &a,int &c){
 int main(){
     // 引用必须有初始值；2，在初始化后不能改变
     // 引用的基本语法
-    int a = 10;
-    int &b = a;
-    int c = 90;
-    b = 30;
+   int a= 10;
+   int b = 20;
     // 引用传递
-    yinyong(a,c);
-    cout << "a的值："<< a<< endl;
-    cout << "b的值："<< b << endl;
+    yinyong(&a,&b);
+    // cout << "a的值："<< a<< endl;
+    // cout << "b的值："<< b << endl;
    
     return 0;
 }
