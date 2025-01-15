17812d19a604a4937d2b3ede1969cdc7d2ee366e
anbo <anbo@runzeyuan.com>
2025-01-15 21:14:33 +0800

    查找

--- set/1.cpp
+++ set/1.cpp
@@ -28,6 +28,9 @@ void isEmpt(set<Person> &s){
         cout << "容器的大小是"<< s.size() << endl;
      }
 };
+void swapSet(set<Person> &s1,set<Person> &s2){
+    s1.swap(s2);
+}
 void insertSet(set<Person> &s){
    Person p1("anbo",20);
    Person p2("lisi",30);
@@ -36,11 +39,23 @@ void insertSet(set<Person> &s){
    s.insert(p3);
    s.insert(p2);
 };
+void insertSetTwo(set<Person> &s){
+   Person p1("anbo1",500);
+   Person p2("lisi1",900);
+   Person p3("wangwu1",540);
+   s.insert(p1);
+   s.insert(p3);
+   s.insert(p2);
+};
 void test(){
     set<Person>s;
+    set<Person>s2;
     insertSet(s);
+    insertSetTwo(s2);
     printSet(s);
     isEmpt(s);
+    swapSet(s,s2);
+    printSet(s);
 };
 int main(){
     test();
--- set/2.cpp
+++ set/2.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 #include <set>
 using namespace std;
+// set容器的查找和统计
+class Person {
+    public:
+    string m_name;
+    int m_age;
+    Person(string name,int age){
+        this->m_age = age;
+        this->m_name = name;
+    };
+    bool operator<(const Person &other)const{
+        return m_age < other.m_age;
+    }
+};
+set<Person>::iterator findSet(const set<Person> &s,const Person &a){
+    return s.find(a);
+};
+void insertSet(set<Person> &s){
+    Person a("安波",20);
+    Person a1("lisi",21);
+    Person a2("lis2",22);
+    s.insert(a);
+    s.insert(a1);
+    s.insert(a2);
+   set<Person>::iterator it= findSet(s,a1);
+    cout << "查找出来的值" << it->m_name << " " << it->m_age << endl;
+};
+void printSet(set<Person>&s){
+    for(set<Person>::iterator it=s.begin();it!=s.end();it++){
+        cout << it->m_name << " " << it->m_age << endl;
+    }
+};
+
+void test(){
+    set<Person>s;
+    insertSet(s);
+    printSet(s);
+}
 int main(){
+    test();
     return 0;
 }
